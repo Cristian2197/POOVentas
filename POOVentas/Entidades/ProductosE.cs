baed7542fa45ffb563f66f1908e60a70b775b2dd
MelissaGarciaMelliza <melissa.garcia1@catolica.edu.sv>
2020-04-22 17:58:59 -0500

    Entidad producto

---
+++ POOVentas/Entidades/ProductosE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class ProductosE
+    {
+        public int id_producto { get; set; }
+        public String nombre { get; set; }
+        public double precio { get; set; }
+        public int stock { get; set; }
+        public int id_categoria { get; set; }
+    }
+}
