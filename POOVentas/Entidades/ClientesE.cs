fa6a81ada5769837db46887a8b10303fe43e0afd
Cristian2197 <50083819+Cristian2197@users.noreply.github.com>
2020-04-22 16:35:40 -0600

    Arreglos Menores

--- POOVentas/Entidades/ClientesE.cs
+++ POOVentas/Entidades/ClientesE.cs
@@ -17,7 +17,9 @@ namespace Entidades
         public string apellido { get; set; }
         [Display(Name = "Dirección del Cliente")]
         public string direccion { get; set; }
+        [DataType(DataType.Date)]
         [Display(Name = "Fecha de Nacimiento")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode =true)]
         public System.DateTime fecha_nacimiento { get; set; }
         [Display(Name = "Telefono")]
         public string telefono { get; set; }
