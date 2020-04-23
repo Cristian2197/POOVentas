f5da6a40999e4857386051e6cf39b139fc94916c
Cristian2197 <50083819+Cristian2197@users.noreply.github.com>
2020-04-23 16:53:51 -0600

    Pantallas productos,clientes y categorias Linkeadas

--- POOVentas/Entidades/CategoriaE.cs
+++ POOVentas/Entidades/CategoriaE.cs
@@ -9,10 +9,13 @@ namespace Entidades
 {
     public class CategoriaE
     {
+        [Required(ErrorMessage ="Es obligatorio poner el ID")]
         [Display(Name ="ID Categoria")]
         public int id_categoria { get; set; }
+        [Required(ErrorMessage = "Es obligatorio poner el Nombre de la Categoria")]
         [Display(Name = "Nombre Categoria")]
         public string nombre { get; set; }
+        [Required(ErrorMessage = "Es obligatorio poner la descripcion")]
         [Display(Name = "Descripcion")]
         public string descripcion { get; set; }
     }
