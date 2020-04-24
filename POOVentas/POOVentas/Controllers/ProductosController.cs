68ba345197f19a9574298d577405b366525e05eb
Cristian2197 <50083819+Cristian2197@users.noreply.github.com>
2020-04-24 16:52:28 -0600

    Ahora ya se pueden ver las litas de las categorias en el apartado producto y se modifico el boton editar productos que se llamaba editar clientes

--- POOVentas/POOVentas/Controllers/ProductosController.cs
+++ POOVentas/POOVentas/Controllers/ProductosController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using AutoMapper;
 using ent = Entidades;
 using dom = Dominio;
+using bd = Datos;
+
 
 
 namespace POOVentas.Controllers
@@ -18,9 +20,28 @@ namespace POOVentas.Controllers
             var _pedido = new dom.ProductosD().ProductosList();
             return View(_pedido);
         }
+        //Codigo que crea la lista
+        List<SelectListItem> ListaCat;
+        private void llenarCat()
+        {
+            using (var BaseDatos = new bd.VentasPOOEntitiesConnection())
+            {
+                ListaCat = (from TablaCat in BaseDatos.CATEGORIA
+                            select new SelectListItem
+                            {
+                                Text = TablaCat.nombre,
+                                Value = TablaCat.id_categoria.ToString()
+                            }).ToList();
+                ListaCat.Insert(0, new SelectListItem { Text = "--Seleccione--", Value = "" });
+            }
+        }
         [HttpGet]
         public ActionResult Crear()
         {
+            
+            //Llenar el combobox
+            llenarCat();
+            ViewBag.lista = ListaCat;
             var _productos = new ent.ProductosE();
             return PartialView("Crear", _productos);
         }
@@ -33,6 +54,8 @@ namespace POOVentas.Controllers
         [HttpGet]
         public ActionResult Editar(int id)
         {
+            llenarCat();
+            ViewBag.lista = ListaCat;
             var _producto = new dom.ProductosD().ProductoPorID(id);
             return PartialView("Editar", _producto);
         }
